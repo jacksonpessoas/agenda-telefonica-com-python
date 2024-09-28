a99cf172cfcc67567c5316a3b1b57d1fd8afd0cb
JacksonPessoaS <jacksonpessoas@gmail.com>
2024-09-28 20:59:10 -0300

    Adiciona código e README para o conversor de planilha Excel para vCard

---
+++ agenda.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import vobject
+
+# Carregar a planilha Excel
+arquivo_excel = 'agenda_telefonica.xlsx'
+planilha = pd.read_excel(arquivo_excel)
+
+# Filtrar as colunas de Nome, Sobrenome (Bairro) e Contato
+agenda = planilha[['Nome', 'Bairro', 'Contato']]  # Usando 'Bairro' como Sobrenome
+
+# Criar um arquivo vCard (.vcf)
+with open('agenda_telefonica.vcf', 'w') as vcf_file:
+    for _, row in agenda.iterrows():
+        vcard = vobject.vCard()
+        
+        # Nome completo e sobrenome
+        nome_completo = row['Nome']
+        sobrenome = row['Bairro']  # Substituindo 'Bairro' por 'Sobrenome'
+        vcard.add('n').value = vobject.vcard.Name(family=sobrenome, given=nome_completo)
+        
+        # Campo 'FN' (nome completo obrigatório)
+        vcard.add('fn').value = f"{nome_completo} {sobrenome}"
+
+        # Telefone
+        vcard.add('tel').value = str(row['Contato'])
+
+        vcf_file.write(vcard.serialize())
+
+print("Arquivo VCF criado com sucesso!")
